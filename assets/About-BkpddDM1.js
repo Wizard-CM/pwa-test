b150cd537d009b3048ce3b1519709f59bde99516
Wizard-CM <ronakthapa37@gmail.com>
2024-11-15 14:49:36 +0545

    changes

---
+++ assets/About-BkpddDM1.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-D1lGhWMN.js";const r=()=>t.jsx("h1",{children:"About"});export{r as default};
--- assets/About-CM7MPnCA.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index--bvG1fm2.js";const r=()=>t.jsx("h1",{children:"About"});export{r as default};
